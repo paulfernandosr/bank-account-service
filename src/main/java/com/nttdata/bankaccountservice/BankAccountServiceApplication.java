d574e4e2ed4544663516216f7776da96d50b5cbf
Paul Fernando Sacaca Ramirez <pfsr98@gmail.com>
2022-07-26 16:53:03 -0500

    update for final project

--- src/main/java/com/nttdata/bankaccountservice/BankAccountServiceApplication.java
+++ src/main/java/com/nttdata/bankaccountservice/BankAccountServiceApplication.java
@@ -1,11 +1,9 @@
 package com.nttdata.bankaccountservice;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@Slf4j
 @EnableEurekaClient
 @SpringBootApplication
 public class BankAccountServiceApplication {
